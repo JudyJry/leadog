2df5e030af6c08954a705f224368d673921eb739
JudyJry <judyjry0127@gmail.com>
2022-01-27 18:32:32 +0800

    update

--- script/YouthAction_Bus.js
+++ script/YouthAction_Bus.js
@@ -33,7 +33,7 @@ class Youth_Bus_Video extends Action.ActionVideo {
         super(manager, action, url);
         this.name = "Youth_Bus_Video";
         //this.pauseTime = [0, 4, 8];
-        this.pauseTime = [0, 16.5, 42];
+        this.pauseTime = [0, 16.5, 45];
         this.isEnd = true;
         this.count = 0;
     }
